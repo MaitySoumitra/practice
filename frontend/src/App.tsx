305956b44ccef70a47dc492ea1912787468fda0e
MaitySoumitra <soumitra.ahaansoftware@gmail.com>
2026-04-09 19:55:27 +0530

    update on productlist

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,8 +14,8 @@ function App() {
     
     return (
         <>
-        <CustomSlider/>
-        {/* <SearchProducts/> */}
+        {/* <CustomSlider/> */}
+        <SearchProducts/>
         {/* <NavBar/> */}
         {/* <TodoList/> */}
         {/* <MenuDelayKeyboard/> */}
--- frontend/src/components/SearchProducts.tsx
+++ frontend/src/components/SearchProducts.tsx
@@ -1,21 +1,24 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
 export const SearchProducts = () => {
     const [search, setSearch] = useState("")
-    const productList = [
-        { name: "Apple", price: 199 },
-        { name: "Mango", price: 100 },
-        { name: "Orange", price: 150 }
-    ]
-    const filterProducts = productList.filter(product =>
-        product.name.toLowerCase().includes(search.toLocaleLowerCase())
+    const [products, setProducts]=useState([])
+    const filterProducts = products.filter(product =>
+        product.title.toLowerCase().includes(search.toLocaleLowerCase())
     )
+    useEffect(()=>{
+        fetch("https://dummyjson.com/products")
+        .then(res=>res.json())
+        .then(data=>{
+            setProducts(data.products)
+        })
+    }, [])
     return (
         <div className="space-y-6 max-w-4xl mx-auto mt-10">
-            <div className="space-x-4">
+            <div className="space-x-4 relative">
                 <input className="p-2 border rounded-md" type="text" onChange={(e) => setSearch(e.target.value)} />
-                <select className="p-2 border rounded-md">
+                <select className="p-2 border rounded-md absolute right-0">
                     <option>Select an Option</option>
                     <option value="All">All</option>
                     <option value="A-Z">A-Z</option>
@@ -25,11 +28,16 @@ export const SearchProducts = () => {
                 </select>
             </div>
 
-            <div className="flex flex-row gap-6">
-                {filterProducts.map((product) => (
-                    <div className="p-4 border rounded shadow-md">
-                        <p>Name: {product.name}</p>
-                        <p>Price: {product.price} </p>
+            <div className="grid grid-cols-2 md:grid-cols-3 lg grid-cols-4 gap-4">
+                {filterProducts.map((p) => (
+                    <div className=" p-4 border rounded shadow-md">
+                        <img src={p.images[0]}/>
+                        <p className="text-xl font-bold">{p.title}</p>
+                        <div className=" flex items-end gap-2">
+                        <p className="font-bold text-base">Rs: {p.discountPercentage}/pcs</p>
+                        <p className="text-sm line-through"> {p.price}/pcs </p>
+                        </div>
+                       
                     </div>
                 ))}
             </div>
