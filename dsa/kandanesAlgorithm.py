6ccae61ddfbc2d3c75541434ddc04e2dbfc6d2ac
MaitySoumitra <soumitra.ahaansoftware@gmail.com>
2026-04-10 19:52:15 +0530

    Binary Search Done

---
+++ dsa/kandanesAlgorithm.py
@@ -0,0 +1,14 @@
+def kandanesAlgorithm(nums, target):
+    left=0
+    right=len(nums)-1
+    while left<=right:
+        mid=(left+right)//2
+
+        if nums[mid]==target:
+            return mid
+        elif target<nums[mid]:
+            right=mid-1
+        else:
+            left=mid+1
+    return left
+print(kandanesAlgorithm([1,3,5,6],7))
